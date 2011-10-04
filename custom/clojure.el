2f3dac1dde00fe815d95451174925decd3edbc84
Ravindra R. Jaju <jaju@msync.org>
2011-10-04 19:21:19 +0530

    Clojure syntax support applied to clojurescript.

--- custom/clojure.el
+++ custom/clojure.el
@@ -7,3 +7,4 @@
 (setq slime-use-autodoc-mode nil)
 (slime-setup)
 (add-hook 'slime-repl-mode-hook 'clojure-mode-font-lock-setup)
+(setq auto-mode-alist (cons '("\\.cljs" . clojure-mode) auto-mode-alist))
