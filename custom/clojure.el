829d3cb32ef1ca2f2ef2e923fd9a7f5804b86dff
Ravindra R. Jaju <jaju@msync.org>
2012-11-03 09:16:00 +0530

    Removed commented out slime config no longer required for clojure.

--- custom/clojure.el
+++ custom/clojure.el
@@ -1,10 +1,5 @@
-;;(require 'slime)
 (require 'clojure-mode)
 
-;(eval-after-load "slime"
-;  '(progn (slime-setup '(slime-repl))))
-;(setq slime-use-autodoc-mode nil)
-;(slime-setup)
 ;(add-hook 'slime-repl-mode-hook 'clojure-mode-font-lock-setup)
 (setq auto-mode-alist (cons '("\\.cljs" . clojure-mode) auto-mode-alist))
 ;(clojure-enable-slime-on-existing-buffers)
