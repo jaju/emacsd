ccb4cb8c49447861abb06863647f085c0770d2c2
Ravindra R. Jaju <jaju@msync.org>
2013-07-09 07:37:51 +0530

    Added a project.clj skeleton generator - skeleton-clojure-project

--- custom/clojure.el
+++ custom/clojure.el
@@ -15,3 +15,47 @@
 (add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
 (eval-after-load "auto-complete"
   '(add-to-list 'ac-modes 'nrepl-mode))
+
+(define-skeleton skeleton-clojure-project
+  "Create a skeleton clojure project.clj"
+  "Name of project: "
+  "(defproject " str " \"0.1.0-SNAPSHOT\""
+  \n >
+  ":description \"\""
+  \n >
+  ":jar-name \"" str ".jar\""
+  \n >
+  ":uberjar-name \"" str "-standalone.jar\""
+  \n \n >
+  ":min-lein-version \"2.2.0\""
+  \n \n >
+  ":source-paths [\"src/clojure\"]"
+  \n >
+  ":java-source-paths [\"src/java\"]"
+  \n >
+  ":test-paths [\"test/clojure\"]"
+  \n >
+  ":resource-paths [\"src/resources\"]"
+  \n \n >
+  ":dependencies [[org.clojure/clojure \"1.5.1\"]]"
+  \n \n >
+  ":profiles {"
+  \n >
+  "           :dev {"
+  \n >
+  "                 :dependencies [[midje \"1.5-alpha5\" :exclusions [org.clojure/clojure]]]"
+  \n >
+  "                 }}"
+  \n \n >
+  ":repl-options {\n"
+  \n >
+  "               :prompt (fn [ns] (str \"[" str " -> \" ns \"]> \"))"
+  \n >
+  "               :init-ns " str ".main"
+  \n >
+  "               :port 60001" \n >
+  "               }" \n \n >
+  ":warn-on-reflection true" \n >
+  ":javac-options [\"-target\" \"1.6\" \"-source\" \"1.6\"]" \n >
+  ":compile-path \"target/classes\"" \n >
+  ")" \n >)
